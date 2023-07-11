544c81a76cf181edbc4f320fcc335adb253cd672
Reena200 <138387440+Reena200@users.noreply.github.com>
2023-07-11 23:26:46 +0530

    Add files via upload

--- Assignment4.py
+++ Assignment4.py
@@ -1,5 +1,4 @@
-#loop mainipulation
-
+#loop mainipulation 
 a="string"
 for i in a:
     print(i)
@@ -10,7 +9,6 @@ for i in 1,2,3,4,5:
 
 
 #or...with else loop using break
-
 a="string"
 for i in a:
     if i=="i":
@@ -21,7 +19,6 @@ else:
       print("this has break")
 
  #for...with else loop using pass
-
 a="string"
 for i in a:
     if i=="i":
@@ -32,7 +29,6 @@ else:
       print("this has pass")    
 
 #for...with else loop using continue
-
 a="string"
 for i in a:
     if i=="i":
@@ -45,7 +41,6 @@ else:
 
 
 #while...with else loop using break
-
 i=1
 while i<=10:
     if i==5:
@@ -57,7 +52,6 @@ else:
    print("this has break")
 
 #while... with else loop  using pass
-
 i=1
 while i<=10:
     if i==5:
@@ -69,91 +63,7 @@ else:
    print("this has pass")
 
 
-#while... with else loop using continue 
-
-i=1
-while i<=10:
-    if i==5:
-        continue
-    else:
-        print(i)
-    i+=1
-else:
-    print("this has continue")
-
-       #loop mainipulation
-
-a="string"
-for i in a:
-    print(i)
-
-a="string"
-for i in 1,2,3,4,5:
-   print(i)
-
-
-#or...with else loop using break
-
-a="string"
-for i in a:
-    if i=="i":
-      break
-    else:
-      print(i)
-else:
-      print("this has break")
-
- #for...with else loop using pass
-
-a="string"
-for i in a:
-    if i=="i":
-         pass
-    else:
-       print(i)
-else:
-      print("this has pass")    
-
-#for...with else loop using continue
-
-a="string"
-for i in a:
-    if i=="i":
-        continue
-    else:
-        print(i)
-else:
-    print("this has continue")    
-
-
-
-#while...with else loop using break
-
-i=1
-while i<=10:
-    if i==5:
-       break
-    else:
-       print(i)
-    i+=1
-else:
-   print("this has break")
-
-#while... with else loop  using pass
-
-i=1
-while i<=10:
-    if i==5:
-        pass
-    else:
-        print(i)
-    i+=1
-else:
-   print("this has pass")
-
-
-#while... with else loop using continue 
- 
+#while... with else loop using continue  
 i=1
 while i<=10:
     if i==5:
